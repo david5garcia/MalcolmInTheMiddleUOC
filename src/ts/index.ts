85734190d28ea1066175f322f1b5e7f3c39d207b
David Garcia Ballester <davidgballester@uoc.edu>
2023-11-22 17:27:33 +0100

    todos los links menos los del header se abren en otra pestaña

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,1 +1,15 @@
-console.log('Archivo de TypeScript carga correctamente!')
+document.addEventListener("DOMContentLoaded", () => {
+  const links = document.querySelectorAll("a:not(header a)");
+
+  links.forEach((link) => {
+    link.addEventListener("click", (event: Event) => {
+      event.preventDefault();
+
+      const href = link.getAttribute("href");
+
+      if (href) {
+        window.open(href, "_blank");
+      }
+    });
+  });
+});
