3440206a77ad2910fbc15d14265a839db1aa6b44
David Garcia Ballester <davidgballester@uoc.edu>
2024-01-16 00:56:36 +0100

    mover logica a archivos js

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,15 +1,19 @@
-function loadIframe(videoId, containerId) {
-  var iframeContainer = document.getElementById(containerId);
+document.addEventListener("DOMContentLoaded", function () {
+  var cargarVideoBtn = document.getElementById("iframe-button");
+  var iframeContainer = document.getElementById("iframeContainer");
 
-  var iframe = document.createElement("iframe");
-  iframe.width = "560";
-  iframe.height = "315";
-  iframe.src = "https://www.youtube.com/embed/" + videoId;
-  iframe.title = "YouTube video player";
-  iframe.frameborder = "0";
-  iframe.allow =
-    "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share";
-  iframe.allowfullscreen = true;
+  cargarVideoBtn.addEventListener("click", function () {
+    const videoSrc = cargarVideoBtn.getAttribute("video-src");
+    var iframe = document.createElement("iframe");
+    iframe.width = "560";
+    iframe.height = "315";
+    iframe.src = "https://www.youtube.com/embed/" + videoSrc;
+    iframe.title = "YouTube video player";
+    iframe.frameborder = "0";
+    iframe.allow =
+      "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share";
+    iframe.allowfullscreen = true;
 
-  iframeContainer.appendChild(iframe);
-}
+    iframeContainer.appendChild(iframe);
+  });
+});
